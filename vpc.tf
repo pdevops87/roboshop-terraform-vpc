5720633dae3a96b73fed90219315a20a2c360a92
Your Name <vemasani.pavani@gmail.com>
2026-05-10 19:05:18 +0530

    add vpc peer connection for default vpc

--- vpc.tf
+++ vpc.tf
@@ -118,6 +118,14 @@ resource "aws_route" "default_vpc_peer" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
 
+resource "aws_route" "default_vpc_peer" {
+  count = length(var.private_subnets)
+  route_table_id = aws_route_table.private[count.index].id
+  destination_cidr_block = var.default_vpc_cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
+
 
 
 
