f7289ac36729e8d1ba4606f58c57bc4cd4757c06
Your Name <vemasani.pavani@gmail.com>
2026-05-10 18:09:53 +0530

    destination cidr block

--- vpc.tf
+++ vpc.tf
@@ -75,7 +75,7 @@ resource "aws_internet_gateway" "igw" {
 # add igw in public route table(routes)
 resource "aws_route" "public" {
   route_table_id            = aws_route_table.public.id
-  destination_cidr_block    = "0.0.0.0"
+  destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.igw.id
 }
 
