9c8e2019cf91f04f1c022f8ad6a1cdcc58da38b7
Your Name <vemasani.pavani@gmail.com>
2026-05-10 22:11:51 +0530

    1. peer connection between two vpc's

--- vpc.tf
+++ vpc.tf
@@ -108,7 +108,7 @@ resource "aws_route" "private_vpc_peer" {
 
 resource "aws_route" "default_vpc_peer" {
   count = length(var.private_subnets)
-  route_table_id = var.default_vpc_cidr_block
+  route_table_id = var.default_route_table_id
   destination_cidr_block =var.private_subnets[count.index]
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
