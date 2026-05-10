02f568d50dc11220571e66ef3501e061f1ad7086
Your Name <vemasani.pavani@gmail.com>
2026-05-10 18:56:53 +0530

    peer connection

--- vpc.tf
+++ vpc.tf
@@ -97,7 +97,7 @@ resource "aws_nat_gateway" "nat" {
 }
 
 # add NAT to private subnets in route table(routes)
-resource "aws_route" "private" {
+resource "aws_route" "private_vpc_peer" {
   count = length(var.private_subnets)
   route_table_id            = aws_route_table.private[count.index].id
   destination_cidr_block    = var.default_vpc_cidr_block
@@ -109,6 +109,13 @@ resource "aws_route" "nat_private" {
   route_table_id = aws_route_table.private[count.index].id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.nat.id
+}
+
+# edit routes for default
+resource "aws_route" "default_vpc_peer" {
+  route_table_id = var.default_route_table_id
+  destination_cidr_block = var.vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
 
 
